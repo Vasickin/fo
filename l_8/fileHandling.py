0fc1220333d6fa6e356b5813017c27c18b0f6223
Lex <vasickin@mail.ru>
2024-04-01 19:37:55 +0300

    Add the user table if it is not created

--- l_8/fileHandling.py
+++ l_8/fileHandling.py
@@ -3,3 +3,13 @@ import sqlite3 as sl
 # Сщздание подключения к БД
 
 conn = sl.connect("FH.db")
+
+# Создание курсора - основного объекта подключения SQL кода
+
+cur = conn.cursor()
+
+# Сосдадим таблицг Users, если она не создана
+
+cur.execute()
+
+
